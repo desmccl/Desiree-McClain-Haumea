fa1609f8398477c238e1709866a1a85da9123764
Desiree Mcclain <desireemcclain51@gmail.com>
2024-04-30 19:38:34 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -18,3 +18,45 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageSection = document.getElementById('messages');
+const messageList = messageSection.querySelector('ul');
+
+const messageForm = document.getElementById('leave_message');
+
+messageSection.style.display = 'none';
+
+leave_message.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = this.usersName.value;
+    const email = this.usersEmail.value;
+    const message = this.usersMessage.value;
+    
+    console.log(name, email, message);
+
+    messageSection.style.display = 'block';
+
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span>${message}</span>`;
+    
+    messageList.appendChild(newMessage);
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove';
+    removeButton.type = 'button';
+
+    removeButton.addEventListener('click', function() {
+        const entry = this.parentNode;
+        entry.remove();
+        
+        if (messageList.childElementCount === 0) {
+        messageSection.style.display = 'none';
+    }
+    });
+
+    newMessage.appendChild(removeButton);
+
+
+    this.reset();
+});
